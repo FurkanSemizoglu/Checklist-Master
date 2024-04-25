81240fbb39bb6fa206f16d0ac78fa3214934500a
Furkan <furkan5590@hotmail.com>
2024-04-25 13:29:25 +0300

    tailwind and shadcn ui added

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/" , (req,res) => {
+    res.send("Server is running")
+})
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URL, {
